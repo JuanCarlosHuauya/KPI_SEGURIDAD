07ae290659c62f4ff4f32cf9da4e71ce72f64308
Juan Carlos Huauya <ing.jchuauya@gmail.com>
2023-12-19 10:46:09 -0500

    primera entrega

---
+++ kpi_2023.py
@@ -0,0 +1,38 @@
+pip install pandas gspread pbpy
+
+import pandas as pd
+import gspread
+from pbpy import Client
+
+# Conexión a Google Drive y acceso al archivo de Excel
+gc = gspread.service_account('path/to/your/service/account/credentials.json')
+sh = gc.open('Your Excel File Name').sheet1
+
+# Lectura de datos de Excel y conexión a Power BI
+data = sh.get_all_records()
+client = Client('path/to/your/power/bi/credentials.json')
+
+# Creación de un conjunto de datos en Power BI
+dataset = client.create_dataset('Your Dataset Name')
+
+# Carga de datos a Power BI
+table = dataset.table('Your Table Name')
+table.upload(data)
+
+# Generación de indicadores en Power BI
+indicators = {
+    'average_revenue': {'column': 'Revenue', 'type': 'Average'},
+    'max_revenue': {'column': 'Revenue', 'type': 'Max'},
+    'min_revenue': {'column': 'Revenue', 'type': 'Min'},
+    'sum_revenue': {'column': 'Revenue', 'type': 'Sum'},
+    'count_orders': {'column': 'OrderID', 'type': 'Count'},
+}
+
+for indicator_name, indicator_details in indicators.items():
+    client.create_indicator(
+        dataset_id=dataset.id,
+        table_name=table.name,
+        indicator_name=indicator_name,
+        indicator_type=indicator_details['type'],
+        column_name=indicator_details['column'],
+    )
