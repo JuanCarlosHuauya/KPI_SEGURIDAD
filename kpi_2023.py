049fcde6de3950cf0559b1de38947b2d3f01e932
Juan Carlos Huauya <ing.jchuauya@gmail.com>
2023-12-19 11:08:13 -0500

    Segunda entrega

--- kpi_2023.py
+++ kpi_2023.py
@@ -1,22 +1,23 @@
 pip install pandas gspread pbpy
 
+
 import pandas as pd
 import gspread
 from pbpy import Client
 
 # Conexión a Google Drive y acceso al archivo de Excel
 gc = gspread.service_account('path/to/your/service/account/credentials.json')
-sh = gc.open('Your Excel File Name').sheet1
+sh = gc.open('reporte_2023_diciembre').sheet1
 
 # Lectura de datos de Excel y conexión a Power BI
 data = sh.get_all_records()
 client = Client('path/to/your/power/bi/credentials.json')
 
 # Creación de un conjunto de datos en Power BI
-dataset = client.create_dataset('Your Dataset Name')
+dataset = client.create_dataset('reporte_makros_surco')
 
 # Carga de datos a Power BI
-table = dataset.table('Your Table Name')
+table = dataset.table('estadistico_sst')
 table.upload(data)
 
 # Generación de indicadores en Power BI
@@ -36,3 +37,6 @@ for indicator_name, indicator_details in indicators.items():
         indicator_type=indicator_details['type'],
         column_name=indicator_details['column'],
     )
+
+# Mostrar los resultados en un dashboar en power bi mediante grafico de lineas y tacometros.
+# Paleta de colores azul degradado
